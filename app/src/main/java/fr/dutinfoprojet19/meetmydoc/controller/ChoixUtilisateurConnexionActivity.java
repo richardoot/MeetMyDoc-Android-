442b5760c43638f2ac375bc39947a9f4ab6d937c
Richard Boilley <richardoot11@gmail.com>
2019-02-06 00:56:38 +0100

    Mise en places basique des activitées ainsi que des layouts de connexion

---
+++ app/src/main/java/fr/dutinfoprojet19/meetmydoc/controller/ChoixUtilisateurConnexionActivity.java
@@ -0,0 +1,15 @@
+package fr.dutinfoprojet19.meetmydoc.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fr.dutinfoprojet19.meetmydoc.R;
+
+public class ChoixUtilisateurConnexionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choix_utilisateur_connexion);
+    }
+}
