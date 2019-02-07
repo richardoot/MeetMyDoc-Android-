fb4d7d35cfa1c93c0578429d56dce02da2de1924
Richard Boilley <richardoot11@gmail.com>
2019-02-07 09:08:12 +0100

    Creation des activité pour l'inscription de patient et medecin v2

---
+++ app/src/main/java/fr/dutinfoprojet19/meetmydoc/controller/InscriptionPatientActivity.java
@@ -0,0 +1,15 @@
+package fr.dutinfoprojet19.meetmydoc.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fr.dutinfoprojet19.meetmydoc.R;
+
+public class InscriptionPatientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inscription_patient);
+    }
+}
